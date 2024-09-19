1f4cc058105a1299db6fc71154995727c0d6a1c2
Anastasiya Kuznetsova <adkuznetsova13@gmail.com>
2024-09-19 12:44:49 +0400

    Update partners_orders.sql

--- partners_orders.sql
+++ partners_orders.sql
@@ -13,7 +13,12 @@ SELECT
     pt.partner_name,
     pt.partner_type,
     pt.commission_rate,
-    (o.total_order_value * pt.commission_rate / 100) AS partner_commission, --make sure to double check commission values
+    CASE
+        -- If the order total is greater than $10,000, apply a special commission rate 2%
+        WHEN o.total_order_value > 10000 THEN (o.total_order_value * (pt.commission_rate + 2) / 100)
+        -- Default commission rate
+        ELSE (o.total_order_value * pt.commission_rate / 100)
+    END AS partner_commission, --make sure to double check commission values
     pt.partner_region,
     s.shipping_status,
     s.shipment_date,
@@ -31,8 +36,6 @@ JOIN
     partners pt ON o.partner_id = pt.partner_id
 LEFT JOIN
     shipping s ON o.order_id = s.order_id
-WHERE
-    pt.partner_type IN ('Affiliate', 'Reseller', 'Distributor')
 ORDER BY
     o.order_date DESC,
     pt.partner_name,
